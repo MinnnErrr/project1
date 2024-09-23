6c74cf02f55a027726166f9f0c46690c58ced8f8
MinnnErrr <lohminer22@gmail.com>
2024-09-23 20:35:04 +0800

    comment

--- dbconfig.php
+++ dbconfig.php
@@ -6,6 +6,7 @@ $password = "";
 
 try{
     $conn = new PDO("mysql:host=$servername; dbname=web1", $user, $password);
+    //set the PDO error mode to execption
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     error_log("Connected successfully", 0);
 }catch(PDOException $e){
